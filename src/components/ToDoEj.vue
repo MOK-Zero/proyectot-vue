d625d53545f055fee03984ff187a54a113c8e74b
MOK-Zero <lgdesentis@gmail.com>
2024-01-18 23:28:46 -0600

    Ejercicio 3 - Terminando logica

--- src/components/ToDoEj.vue
+++ src/components/ToDoEj.vue
@@ -1,12 +1,21 @@
 <template>
 
     <div>
-        <h2>Lista de Tareas <button @click="agregarTarea('Tarea Nueva', 'Descripcion')">Agregar Nueva</button></h2> <!--- TODO: Definir estilos sobre la lisa -->
+        <h2>Lista de Tareas <button @click="agregarTarea('Tarea Nueva', 'Descripcion')">Agregar Nueva</button></h2>
+        <h2>Agregue un titulo: </h2> <input type="text" v-model="nuevoTitulo">
+        <h2>Agregue una descripcion: </h2> <input type="text" v-model="nuevaDescripcion">
+        <button @click="agregarTarea(nuevoTitulo, nuevaDescripcion)">Agregar una nueva tarea</button>
+        <h1>Indice a editar {{editando}}</h1>
         <ul v-if=" listaTareas.length>0 ">
-            <li v-for="tarea in listaTareas" :key="tarea.id"> <!-- Si la lista esta vacia no se ejecuta el For -->
-                <span>{{ tarea.titulo }}</span> - <span>{{ tarea.descripcion }}</span> - {{ tarea.estado ? 'Completada' : 'Pendiente' }}
-                <button> Editar Tarea </button> <!--- TODO: Habiliitar la funcionaliidad de edicion, se recomienda agregar la propiedad editable a los nodos SPAN -->
+            <li v-for="(tarea, index) in listaTareas" :key="tarea.id">
+              <span> {{tarea.id}} {{ tarea.titulo }}</span> - <span>{{ tarea.descripcion }}</span>
+              <h1 v-if="index === editando">Estamos editando {{tarea.id}}</h1>
+
+                <button @click="completarTarea(tarea.id)">{{ tarea.estado ? 'Completada' : 'Pendiente' }}</button>
+                <button @click="iniciarEdicionTarea(tarea.id, index)"> Editar Tarea {{ editando }}</button> 
+                <!--- TODO: Habiliitar la funcionaliidad de edicion, se recomienda agregar la propiedad editable a los nodos SPAN -->
                 <button @click="eliminarTarea(tarea.id)">X</button>
+
             </li>
         </ul>
         <div v-else>
@@ -21,6 +30,10 @@
 
     const listaTareas = ref([])
 
+    const nuevoTitulo = ref("")
+    const nuevaDescripcion = ref("")
+    const editando = ref(-1)
+
     console.log("CONTENIDO LISTA DE TAREAS AL CREAR")
     console.log(listaTareas)
 
@@ -28,9 +41,13 @@
     // Agrega las tareas a la lista
     function agregarTarea(titulo, descripcion){
         if( (titulo != undefined && titulo != "") ){
-            const id = listaTareas.length ? listaTareas.length + 1 : 0
+            // const id = listaTareas.value.length ? listaTareas.value.length + 1 : 0
+            const id = listaTareas.value.length + 1
             const nuevaTarea = new Tarea(id, titulo, descripcion)
             listaTareas.value.push(nuevaTarea)
+
+            nuevaDescripcion.value = ""
+            nuevoTitulo.value = ""
 
         }else{
             alert("El titulo debe contener datos")
@@ -53,8 +70,22 @@
 
     }
 
+    function iniciarEdicionTarea(id, index){
+        const tarea = listaTareas.value.find(tarea => tarea.id === id);
+        if(tarea){
+            nuevoTitulo.value = tarea.titulo
+            nuevaDescripcion.value = tarea.descripcion
+            editando.value = index
+            
+            console.log("Valor de los atributos es : ", tarea.titulo, tarea.descripcion)
+            console.log("Los valores auxiliares son:  Titulo -> ", nuevoTitulo.value," y  Desc -> ",nuevaDescripcion.value)
+        }else{
+            alert("No se encontró ninguna tarea con el id"+id)
+        }
+    }
+
     function editarTarea(id, nuevoTitulo, nuevaDescripcion, nuevoEstado){
-        const tarea = listaTareas.find(tarea => tarea.id === id);
+        const tarea = listaTareas.value.find(tarea => tarea.id === id);
         if(tarea){
             if(nuevoTitulo != undefined && nuevoTitulo != ""){
                 tarea.titulo = nuevoTitulo;
@@ -70,10 +101,10 @@
     }
 
     // El estado cambiara con un checkbox y solo tendra true o false
-    function completarTarea(id, nuevoEstado){
-        const tarea = listaTareas.find(tarea => tarea.id === id);
+    function completarTarea(id){
+        const tarea = listaTareas.value.find(tarea => tarea.id === id);
         if(tarea){
-            tarea.estado = nuevoEstado
+            tarea.estado = !tarea.estado
         }
     }
 
